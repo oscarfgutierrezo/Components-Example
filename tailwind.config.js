d0f9872ea9e58e5d3a3ebf4024c79dd143e29925
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-03-03 16:05:58 -0500

    Add: button section, input-field section, dropdowns section

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        'sans': ['DM Sans', 'sans-serif'],
+      },
+      extend: {
+        colors: {
+          "primary": "#2F1893",
+          "green": "#25DAC5",
+          "violet": "#1E0E62",
+          "dark-violet": "#151439",
+          "bright-gray": "#EBEAED",
+        }
+      }
+    },
+    plugins: [],
+};
