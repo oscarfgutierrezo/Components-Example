cdb1f01e14c88ab737e153d5d33df7e779094312
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-03-07 11:23:52 -0500

    Finished product

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const theme = require('tailwindcss/defaulttheme')
+
 module.exports = {
     content: ["./*.{html,js}"],
     theme: {
@@ -10,12 +12,17 @@ module.exports = {
       extend: {
         colors: {
           "primary": "#2F1893",
+          "background": "#F2F2F2",
           "green": "#25DAC5",
+          "dark-green": "#1EAE9E",
           "violet": "#1E0E62",
           "dark-violet": "#151439",
           "bright-gray": "#EBEAED",
+          "blue-focus": "#2563EB"
         }
       }
     },
-    plugins: [],
+    plugins: [
+      require('@tailwindcss/forms'),
+    ],
 };
